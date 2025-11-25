2bcb0bfe4255fcd2b33cf3f17b0ecca0a65064d3
Miner320 <pedrobsb0303@gmail.com>
2025-11-25 14:31:20 -0300

    mudanças para os graficos

--- aux.py
+++ aux.py
@@ -1,6 +1,8 @@
 import sqlite3
 from bs4 import BeautifulSoup as Soup
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
 
 DB_file = "sampleDB.db"
 
@@ -28,26 +30,25 @@ def RetrieveSensorData(sensorId):
         connection = sqlite3.connect(DB_file)
         cursor = connection.cursor()
 
-        cursor.execute(f"SELECT Valor FROM Registros WHERE (Sensor={sensorId} AND Tipo=1);")
+        cursor.execute(f"SELECT Valor FROM Registros WHERE (Sensor={sensorId} AND Tipo=1) ORDER BY TimeStamp DESC LIMIT 5;")
         DataList.append(cursor.fetchall())
 
-        cursor.execute(f"SELECT TimeStamp FROM Registros WHERE (Sensor={sensorId} AND Tipo=1);")
+        cursor.execute(f"SELECT TimeStamp FROM Registros WHERE (Sensor={sensorId} AND Tipo=1) ORDER BY TimeStamp DESC LIMIT 5;")
         DataList.append(cursor.fetchall())
 
-        cursor.execute(f"SELECT Valor FROM Registros WHERE (Sensor={sensorId} AND Tipo=2);")
+        cursor.execute(f"SELECT Valor FROM Registros WHERE (Sensor={sensorId} AND Tipo=2) ORDER BY TimeStamp DESC LIMIT 5;")
         DataList.append(cursor.fetchall())
 
-        cursor.execute(f"SELECT TimeStamp FROM Registros WHERE (Sensor={sensorId} AND Tipo=2);")
+        cursor.execute(f"SELECT TimeStamp FROM Registros WHERE (Sensor={sensorId} AND Tipo=2) ORDER BY TimeStamp DESC LIMIT 5;")
         DataList.append(cursor.fetchall())
 
-        cursor.execute(f"SELECT Valor FROM Registros WHERE (Sensor={sensorId} AND Tipo=3);")
+        cursor.execute(f"SELECT Valor FROM Registros WHERE (Sensor={sensorId} AND Tipo=3) ORDER BY TimeStamp DESC LIMIT 5;")
         DataList.append(cursor.fetchall())
 
-        cursor.execute(f"SELECT TimeStamp FROM Registros WHERE (Sensor={sensorId} AND Tipo=3);")
+        cursor.execute(f"SELECT TimeStamp FROM Registros WHERE (Sensor={sensorId} AND Tipo=3) ORDER BY TimeStamp DESC LIMIT 5;")
         DataList.append(cursor.fetchall())
 
         connection.close()
-        print(DataList)
         return DataList
 
     except sqlite3.Error as e:
@@ -105,31 +106,42 @@ def MakeGraphsForSensor(sensorId):
 
     ValoresTipo1 = DataList[0]
     TimesTipo1 = DataList[1]
+    DateTimes1 = [datetime.fromtimestamp(t[0]) for t in TimesTipo1]
 
-    plt.plot(TimesTipo1, ValoresTipo1)
-    plt.xlabel("Tempo (s)")
+    print(DateTimes1)
+
+    plt.plot(DateTimes1, ValoresTipo1)
+    plt.xlabel("Mes-Dia Hora:Minuto")
     plt.ylabel("Valor")
     plt.title("Medidas de temperatura")
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
+    plt.gcf().autofmt_xdate()
     plt.savefig(f"static/tempGraph/{sensorId}.png")
     plt.clf()
 
     ValoresTipo2 = DataList[2]
     TimesTipo2 = DataList[3]
+    DateTimes2 = [datetime.fromtimestamp(t[0]) for t in TimesTipo2]
 
-    plt.plot(TimesTipo2, ValoresTipo2)
-    plt.xlabel("Tempo (s)")
+    plt.plot(DateTimes2, ValoresTipo2)
+    plt.xlabel("Mes-Dia Hora:Minuto")
     plt.ylabel("Valor")
     plt.title("Medidas de poeira")
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
+    plt.gcf().autofmt_xdate()
     plt.savefig(f"static/dustGraph/{sensorId}.png")
     plt.clf()
 
     ValoresTipo3 = DataList[4]
     TimesTipo3 = DataList[5]
+    DateTimes3 = [datetime.fromtimestamp(t[0]) for t in TimesTipo3]
 
-    plt.plot(TimesTipo3, ValoresTipo3)
-    plt.xlabel("Tempo (s)")
+    plt.plot(DateTimes3, ValoresTipo3)
+    plt.xlabel("Mes-Dia Hora:Minuto")
     plt.ylabel("Valor")
     plt.title("Medidas de umidade")
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
+    plt.gcf().autofmt_xdate()
     plt.savefig(f"static/umiGraph/{sensorId}.png")
     plt.clf()
 
